2c1befbe86c31fa956294f30a62356f1ee10e940
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2020-12-19 21:06:31 +0100

    Improve error handling

--- lib/pw_project/router.ex
+++ lib/pw_project/router.ex
@@ -1,7 +1,7 @@
 defmodule PwProject.Router do
   import Plug.Conn
   use Plug.Router
-  require Logger
+  use Plug.ErrorHandler
 
   plug(:match)
   plug(:dispatch)
@@ -11,13 +11,30 @@ defmodule PwProject.Router do
   end
 
   get "/remove" do
-    conn = Plug.Conn.fetch_query_params(conn)
-    %{"string" => string, "char" => char} = conn.query_params
-    response = String.replace(string, char, "")
-    send_resp(conn, 200, response)
+    conn = fetch_query_params(conn)
+
+    try do
+      %{"string" => string, "char" => char} = conn.query_params
+      if String.length(char) != 1, do: throw(char)
+      {string, char}
+    rescue
+      MatchError ->
+        send_resp(conn, 400, "Bad request!")
+    catch
+      char ->
+        send_resp(conn, 400, "Bad request! '#{char}' is not one character!")
+    else
+      {string, char} ->
+        response = String.replace(string, char, "")
+        send_resp(conn, 200, response)
+    end
   end
 
   match _ do
     send_resp(conn, 404, "Not found!")
+  end
+
+  def handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack}) do
+    send_resp(conn, conn.status, "Something went wrong!")
   end
 end
